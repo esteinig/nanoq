1666ea6a98d620f395ae7d0cb3f773ba9525b8a3
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-03-06 23:59:54 +1000

    bio::io [closes #1]; speed [closes #2]; stats [closes #3]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::{Arg, ArgMatches, App};
-use fastq::{parse_path, Record};
-
+use std::cmp::Ordering;
+use bio::io::fastq;
 use std::str;
 use libm::log10;
+
+
 
 fn command_line_interface<'a>() -> ArgMatches<'a> {
 
@@ -18,60 +20,90 @@ fn command_line_interface<'a>() -> ArgMatches<'a> {
 }
 
 fn main() {
-    
+
     let cli = command_line_interface();
 
-    let filename = match cli.value_of("fastq") {
-        None | Some("-") => { None },
-        Some(name) => Some(name)
+    let reader = match cli.value_of("fastq") {
+        Some(filename) => fastq::Reader::from_file(filename).expect("Could not initiate reader from file"),
+        None => panic!("Input file must be specified with --fastq")
     };
 
     let min_length: u64 = cli.value_of("length").unwrap_or("0").parse().unwrap();
     let min_quality: f64 = cli.value_of("quality").unwrap_or("0").parse().unwrap();
 
-    parse_path(filename, |parser| {
-                
-        parser.each( |record| {
-            
-            let seq = match str::from_utf8(record.seq()) {
-                Ok(v) => v, Err(e) => panic!("Invalid UTF-8 sequence: {}", e)
-            };
+    let mut basepairs: u64 = 0;
+    let mut reads: u64 = 0;
+    let mut read_lengths: Vec<u64> = Vec::new();
+    let mut read_qualities: Vec<f64> = Vec::new();
 
-            let head = match str::from_utf8(record.head()) {
-                Ok(v) => v, Err(e) => panic!("Invalid UTF-8 sequence: {}", e)
-            };
-
-            let qual = match str::from_utf8(record.qual()) {
-                Ok(v) => v, Err(e) => panic!("Invalid UTF-8 sequence: {}", e)
-            };
-
-            let seq_len = seq.len() as u64;
-            
-            // Nanopore quality score computation
-
-            let error_probabilities: Vec<f64> = qual.chars().map(|x| get_error_probability(x)).collect();
-            let mean_error: f64 = mean(&error_probabilities);
-            let mean_quality: f64 = -10f64*log10(mean_error);
-
-            if seq_len >= min_length && mean_quality >= min_quality{
-                println!("@{}\n{}\n+\n{}", head, seq, qual);
-            }            
-
-            true
-
-        }).expect("Invalid fastq file");
+    for result in reader.records() {
         
-    }).expect("Invalid compression");
+        let record = result.expect("Could not parse read record");
+
+        let head = record.id();
+
+        let seq = match str::from_utf8(record.seq()) {
+            Ok(v) => v, Err(e) => panic!("Invalid sequence: {}", e)
+        };
+
+        
+        // // Nanopore quality score computation
+
+        let quality_values: Vec<u8> = record.qual().to_vec(); // .iter().map(|x| 10f64.powf(*x as f64 / -10f64) ).collect();
+        
+        let mean_error = get_mean_error(&quality_values);
+        let mean_quality: f64 = -10f64*log10(mean_error as f64);
+
+        let seq_len = seq.len() as u64;
+
+        read_lengths.push(seq_len);
+        read_qualities.push(mean_quality);
+        
+        // // Nanopore quality score computation
+        
+        basepairs += seq_len;
+        reads += 1;
+
+        // if seq_len >= min_length && seq_len > 0 && mean_quality >= min_quality && min_quality > 0.0 {
+        //     println!("@{}\n{}\n+\n{}", head, seq, qual);
+        // }           
+
+    }  
+
+    // Summary statistics
+
+    let mean_read_length = get_mean_read_length(&read_lengths);
+    let mean_read_quality = get_mean_read_quality(&read_qualities);
+    let median_read_length = get_median_read_length(&mut read_lengths);
+    let median_read_quality = get_median_read_quality(&mut read_qualities);
+    let (min_read_length, max_read_length) = get_read_length_range(&read_lengths);
+
+    eprintln!("{:} {:} {:} {:} {:} {:} {:.2} {:.2}", basepairs, reads, max_read_length, min_read_length, mean_read_length, median_read_length, mean_read_quality, median_read_quality);
     
 }
 
 // Helper functions
 
-fn get_error_probability(ascii: char) -> f64 {
+fn compare_f64(a: &f64, b: &f64) -> Ordering {
+
+    // Will get killed with NAN, but we 
+    // should never see NAN
+
+    if a < b {
+        return Ordering::Less;
+    } else if a > b {
+        return Ordering::Greater;
+    }
+    Ordering::Equal
+}
+
+fn get_mean_error(quality_bytes: &Vec<u8>) -> f32 {
 
     /* Compute the error probability from the quality score of a single base
 
-    Quality encoding: Sanger Phred+33 --> ASCII: 36 - 126 --> Q: 0 - 93
+    Quality encoding: Sanger Phred+33 --> ASCII: 33 - 126 --> Q: 0 - 93
+
+    f32 vs f64 makes a huge difference!
 
     Computation of the base quality scores is described at:
 
@@ -80,24 +112,76 @@ fn get_error_probability(ascii: char) -> f64 {
     https://gigabaseorgigabyte.wordpress.com/2017/06/26/averaging-basecall-quality-scores-the-right-way/
 
     */
-
-    let sanger_phred = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
     
-    match sanger_phred.find(ascii) {
-        Some(v) => {
-            10f64.powf(v as f64 / -10f64)
-        }, 
-        None => panic!("Invalid quality encoding: {}", ascii)
+    let mut sum: f32 = 0.0;
+    for q in quality_bytes.iter(){
+        sum += 10f32.powf((q-33u8) as f32 / -10f32)  // Translate to Q-score and error probability
+    }
+    
+    sum / quality_bytes.len() as f32  // Mean error probability
+
+}
+
+// Read length range
+
+fn get_read_length_range(numbers: &Vec<u64>) -> (&u64, &u64) {
+
+    let min_read_length = numbers.iter().min().expect("Could not determine minimum read length");
+    let max_read_length = numbers.iter().max().expect("Could not determine maximum read length");
+    
+    return (min_read_length, max_read_length)
+
+}
+
+// Mean and medians for different numeric types
+
+fn get_median_read_length(numbers: &mut Vec<u64>) -> u64 {
+    
+    // Compute the median of a vector of unsigned integers
+
+    numbers.sort();
+
+    let mid = numbers.len() / 2;
+    if numbers.len() % 2 == 0 {
+        get_mean_read_length(&vec![numbers[mid - 1], numbers[mid]]) as u64
+    } else {
+        numbers[mid]
     }
 
 }
 
-fn mean(errors: &Vec<f64>) -> f64 {
+fn get_mean_read_length(numbers: &Vec<u64>) -> u64 {
+
+    // Compute the mean of a vector of unsigned integers
+
+    let sum: u64 = numbers.iter().sum();
+
+    sum as u64 / numbers.len() as u64
+
+}
+
+
+fn get_median_read_quality(numbers: &mut Vec<f64>) -> f64 {
+
+    // Compute the median of a vector of double-precision floats
+
+    numbers.sort_by(compare_f64);
+
+    let mid = numbers.len() / 2;
+    if numbers.len() % 2 == 0 {
+        get_mean_read_quality(&vec![numbers[mid - 1], numbers[mid]]) as f64
+    } else {
+        numbers[mid]
+    }
+
+}
+
+fn get_mean_read_quality(numbers: &Vec<f64>) -> f64 {
 
     // Compute the mean of a vector of double-precision floats
 
-    let sum: f64 = errors.iter().sum();
+    let sum: f64 = numbers.iter().sum();
 
-    sum as f64 / errors.len() as f64
+    sum as f64 / numbers.len() as f64
 
 }
