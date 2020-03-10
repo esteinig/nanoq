568cb3dcb2796ef9d209e7e099fd6c8829c18c0d
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2020-03-11 08:18:18 +1000

    closes #7; closes #8

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use clap::{Arg, ArgMatches, App};
+use std::io::{self, BufReader, BufWriter, Read, Write};
 use std::cmp::Ordering;
 use bio::io::fastq;
-use std::path::Path;
 use std::fs;
-use std::io;
+use std::process;
 use libm::log10;
 
 fn command_line_interface<'a>() -> ArgMatches<'a> {
 
-    // Sets the command line interface of the program.
+    // Sets the command line interface of the program
 
     App::new("nanoq")
             .version("0.0.1")
-            .about("\nMinimal quality control for nanopore reads\n")
-            .arg(Arg::from_usage("-f, --fastq=[FILE] 'Input fastq file [required]'"))
-            .arg(Arg::from_usage("-o, --output=[FILE] 'Output fastq file [required].'"))
+            .about("\nMinimal quality control and summary statistics for nanopore reads\n")
+            .arg(Arg::from_usage("-f, --fastq=[FILE] 'Input fastq file [-]'"))
+            .arg(Arg::from_usage("-o, --output=[FILE] 'Output fastq file [-]'"))
             .arg(Arg::from_usage("-l, --length=[INT] 'Minimum read length [0]'"))
             .arg(Arg::from_usage("-q, --quality=[INT] 'Minimum read quality [0]'"))
             .get_matches()
@@ -24,23 +24,21 @@ fn main() {
 
     let cli = command_line_interface();
  
-    let reader = match cli.value_of("fastq") {
-        Some(filename) => fastq::Reader::from_file(filename).expect("Could not initiate reader from file"),
-        None => panic!("Error: input file must be specified with --fastq")
+    let input_handle: Box<dyn Read> = match cli.value_of("fastq") {
+        Some(filename) if filename != "-" => Box::new(fs::File::open(filename).unwrap()),
+        _ => Box::new(BufReader::new(io::stdin()))
     };
 
-    let path = match cli.value_of("output") {
-        Some(filename) => Path::new(filename),
-        None => panic!("Error: output file must be specified with --output")
+     let output_handle: Box<dyn Write> = match cli.value_of("output") {
+        Some(filename) if filename != "-" => Box::new(fs::File::create(filename).unwrap()),
+        _ => Box::new(BufWriter::new(io::stdout()))
     };
-    
+
     let min_length: u64 = cli.value_of("length").unwrap_or("0").parse().unwrap();
     let min_quality: f64 = cli.value_of("quality").unwrap_or("0").parse().unwrap();
-
-    let file = fs::File::create(path).unwrap();
-
-    let handle = io::BufWriter::new(file);
-    let mut writer = fastq::Writer::new(handle);
+    
+    let reader = fastq::Reader::new(input_handle);
+    let mut writer = fastq::Writer::new(output_handle);
 
     let mut basepairs: u64 = 0;
     let mut reads: u64 = 0;
@@ -49,8 +47,8 @@ fn main() {
 
     for result in reader.records() {
         
-        let record = result.expect("Error: could not parse record");
-        
+        let record = result.expect("Error: could not read record");
+
         // Nanopore quality score computation
 
         let quality_values: Vec<u8> = record.qual().to_vec();
@@ -58,20 +56,28 @@ fn main() {
         let mean_quality: f64 = -10f64*log10(mean_error as f64);
 
         let seq_len = record.seq().len() as u64;
-
-        read_lengths.push(seq_len);
-        read_qualities.push(mean_quality);
-        
-        basepairs += seq_len;
-        reads += 1;
-
+                
         if seq_len >= min_length && mean_quality >= min_quality {
-            writer.write_record(&record).expect("Error: could not write record.");
+            
+            read_lengths.push(seq_len);
+            read_qualities.push(mean_quality);            
+            basepairs += seq_len;
+            reads += 1;
+
+            if min_length > 0 || min_quality > 0.0 {
+                // Write only when filters are set, otherwise compute stats only
+                writer.write_record(&record).expect("Error: could not write record.");
+            }
         }           
 
     }  
 
     // Summary statistics
+
+    if reads == 0 {
+        eprintln!("No reads");
+        process::exit(1);
+    }
 
     let mean_read_length = get_mean_read_length(&read_lengths);
     let mean_read_quality = get_mean_read_quality(&read_qualities);
@@ -126,7 +132,7 @@ fn get_mean_error(quality_bytes: &Vec<u8>) -> f32 {
     
     let mut sum: f32 = 0.0;
     for q in quality_bytes.iter(){
-        sum += 10f32.powf((q-33u8) as f32 / -10f32)  // Translate to Q-score and error probability
+        sum += 10f32.powf((q-33u8) as f32 / -10f32)  // Q score and error probability
     }
     
     sum / quality_bytes.len() as f32  // Mean error probability
