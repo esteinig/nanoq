699fdfbac18782936c2e91cbcfbefc8207d04a1c
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-18 11:23:05 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn crabcast(fastx: String, output: String, min_length: u64, max_length: u64, min
         fastq::Reader::new(File::open(&fastx)?);
     };
 
-    let mut writer = fastq::Writer::new(File::create(&output)?);
+    let mut writer <fastq::Writer> = fastq::Writer::new(File::create(&output)?);
     
     let max_length = if max_length <= 0 { u64::MAX } else { max_length };
 
