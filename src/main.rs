26a39456e926befda45abd701a64288c65654d9f
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-18 16:57:34 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -257,13 +257,13 @@ fn two_pass_filter(fastx: String, keep_percent: f64, keep_bases: usize){
 
 // Base functions
 
-fn eprint_stats(reads: u64, base_pairs: u64, read_lengths: Vec<u64>, read_qualities: Vec<f64>) -> Result<(), Error> {
+fn eprint_stats(reads: u64, base_pairs: u64, mut read_lengths: Vec<u64>, mut read_qualities: Vec<f64>) -> Result<(), Error> {
 
     let mean_read_length = get_mean_read_length(&read_lengths);
     let mean_read_quality = get_mean_read_quality(&read_qualities);
-    let median_read_length = get_median_read_length(&mut read_lengths);
-    let median_read_quality = get_median_read_quality(&mut read_qualities);
-    let read_length_n50 = get_read_length_n50(&base_pairs, &mut read_lengths);
+    let median_read_length = get_median_read_length(&mread_lengths);
+    let median_read_quality = get_median_read_quality(&read_qualities);
+    let read_length_n50 = get_read_length_n50(&base_pairs, &read_lengths);
     let (min_read_length, max_read_length) = get_read_length_range(&read_lengths);
 
     eprintln!(
