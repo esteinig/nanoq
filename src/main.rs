fdbb52a11ac236afba26c97d72f9705fa02731cd
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-18 17:37:30 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -294,9 +294,10 @@ fn is_fastq(fastx: &String) -> Result<bool, Error> {
         parse_fastx_reader(File::open(&fastx)?).expect("invalid file")
     };
 
-    let first_read = reader.next().unwrap().format();
+    let first_read = reader.next()?;
+    let read_format = first_read.unwrap().format();
 
-    println!("{:?}", first_read);
+    println!("{:?}", read_format);
 
     Ok(true)
 }
