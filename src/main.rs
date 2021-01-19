84b4aaef74f46123c2a192b563ad3cbb90fed0e6
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-19 14:33:12 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -543,53 +543,53 @@ fn get_read_length_n50(base_pairs: &u64, read_lengths: &mut Vec<u64>) -> u64 {
 mod tests {
     use super::*;
 
-    let empty_u64: Vec<u64> = !vec[];
-    let empty_f64: Vec<f64> = !vec[];
+    const U64_EMPTY: Vec<u64> = !vec[];
+    const F64_EMPTY: Vec<f64> = !vec[];
 
-    let f64_values_even: Vec<f64> = !vec[10., 10., 20., 30.];
-    let f64_values_odd: Vec<f64>  = !vec[10., 10., 20., 30., 40.];
-    let u64_values_even: Vec<u64> = !vec[10, 10, 20, 30];
-    let u64_values_odd: Vec<u64>  = !vec[10, 10, 20, 30, 40];
+    const F64_EVEN: Vec<f64> = !vec[10., 10., 20., 30.];
+    const F64_ODD: Vec<f64>  = !vec[10., 10., 20., 30., 40.];
+    const U64_EVEN: Vec<u64> = !vec[10, 10, 20, 30];
+    const U64_ODD: Vec<u64>  = !vec[10, 10, 20, 30, 40];
 
     #[test]
     fn test_get_read_length_n50() {
-        let n50 = get_read_length_n50(70, u64_values_even);
+        let n50 = get_read_length_n50(70, U64_EVEN);
         assert_eq!(n50, 20 as u64);
     }
 
     #[test]
     fn test_get_mean_read_quality() {
-        let mean_quality = get_mean_read_quality(f64_values_even);
+        let mean_quality = get_mean_read_quality(F64_EVEN);
         assert_eq!(mean_quality, 35 as f64);
     }
 
     #[test]
     fn test_get_median_read_quality_even() {
-        let median_quality = get_median_read_quality(f64_values_even);
+        let median_quality = get_median_read_quality(F64_EVEN);
         assert_eq!(median_quality, 15 as f64);
     }
 
     #[test]
     fn test_get_median_read_quality_odd() {
-        let median_quality = get_median_read_quality(f64_values_odd);
+        let median_quality = get_median_read_quality(F64_ODD);
         assert_eq!(median_quality, 20 as f64);
     }
 
     #[test]
     fn test_get_median_read_length_even() {
-        let median_quality = get_median_read_quality(u64_values_even);
+        let median_quality = get_median_read_quality(U64_EVEN);
         assert_eq!(median_quality, 15 as u64);
     }
 
     #[test]
     fn test_get_median_read_length_odd() {
-        let median_quality = get_median_read_quality(u64_values_odd);
+        let median_quality = get_median_read_quality(U64_ODD);
         assert_eq!(median_quality, 20 as u64);
     }
 
     #[test]
     fn test_get_read_length_range() {
-        let (min, max) = get_median_read_quality(u64_values_even);
+        let (min, max) = get_median_read_quality(U64_EVEN);
         assert_eq!(min, 10 as u64);
         assert_eq!(max, 30 as u64);
     }
