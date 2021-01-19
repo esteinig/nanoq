0600ff295d79e949ede456741fd2a24cd382908a
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-19 12:19:19 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -328,8 +328,7 @@ fn two_pass_filter(fastx: String, output: String, keep_percent: f64, keep_bases:
 
     // Second pass, filter reads to output by indices
     let mut _indices: HashMap<usize, f64> = indexed_qualities_retain.iter().cloned().collect();
-
-    needlecast_filt(fastx, output, _indices);
+    needlecast_filt(&fastx, output, _indices);
 
 }
 
