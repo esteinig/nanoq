603d1e2030b795819d1956f4bdff6f03e191050b
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-20 13:53:56 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -546,15 +546,9 @@ mod tests {
     // Mean error read score (Q)
 
     #[test]
-    fn test_mean_error_high() {
-        let error = get_mean_error(b"????????????????????????????");
-        assert_eq!(error, 30 as f32);
-    }
-
-    #[test]
-    fn test_mean_error_low() {
-        let error = get_mean_error(b"IIIIIIJJJJJJ");
-        assert_eq!(error, 0.00008971642 as f32);
+    fn test_mean_error() {
+        let error = get_mean_error(b"???");
+        assert_eq!(error, 0.001 as f32);
     }
 
     // N50
