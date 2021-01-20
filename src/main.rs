2b60561d2a46694537eec4e8e6139b7e47ffcc73
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-20 13:11:55 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -546,16 +546,16 @@ mod tests {
     // N50
 
     #[test]
-    fn test_read_length_n50() {
-        let n50 = get_read_length_n50(&70, &mut vec![10, 10, 20, 30]);
-        assert_eq!(n50, 20 as u64);
+    #[should_panic]
+    fn test_read_length_n50_empty() {
+        let mut test_data: Vec<u64> = Vec::new();
+        let n50 = get_read_length_n50(&70, &test_data);
     }
 
     #[test]
-    #[should_panic]
-    fn test_read_length_n50_empty() {
-        let test_data: Vec<u64> = Vec::new();
-        let n50 = get_read_length_n50(&70, &mut test_data);
+    fn test_read_length_n50() {
+        let n50 = get_read_length_n50(&70, &mut vec![10, 10, 20, 30]);
+        assert_eq!(n50, 20 as u64);
     }
 
     // Read quality
@@ -563,8 +563,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_mean_read_quality_empty() {
-        let test_data: Vec<f64> = Vec::new();
-        let mean_quality = get_mean_read_quality(&mut test_data);
+        let mut test_data: Vec<f64> = Vec::new();
+        let mean_quality = get_mean_read_quality(&test_data);
     }    
 
     #[test]
@@ -577,8 +577,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_median_read_quality_empty() {
-        let test_data: Vec<f64> = Vec::new();
-        let median_quality = get_median_read_quality(&mut test_data);
+        let mut test_data: Vec<f64> = Vec::new();
+        let median_quality = get_median_read_quality(&test_data);
     }    
 
     #[test]
@@ -598,7 +598,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_mean_read_length_empty() {
-        let test_data: Vec<u64> = Vec::new();
+        let mut test_data: Vec<u64> = Vec::new();
         let mean_length = get_mean_read_length(&test_data);
     }
 
@@ -611,8 +611,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_median_read_length_empty() {
-        let test_data: Vec<u64> = Vec::new();
-        let median_length = get_median_read_length(&mut test_data);
+        let mut test_data: Vec<u64> = Vec::new();
+        let median_length = get_median_read_length(&test_data);
     }
 
     #[test]
