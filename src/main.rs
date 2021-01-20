987f74e5bf1743ed6bb6a135167b128874eacc3f
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-20 13:41:45 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -564,7 +564,7 @@ mod tests {
     fn test_mean_read_quality_empty() {
         let mut test_data: Vec<f64> = Vec::new();
         let mean_quality = get_mean_read_quality(&test_data);
-        assert_eq!(mean_quality, NaN);
+        assert!(mean_quality.is_none());
     }    
 
     #[test]
