1bb1f86e9c7a45dc6ab2ad2afacfa33c411e27ee
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-21 21:24:28 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -591,7 +591,7 @@ mod tests {
     
         let read_qualities: Vec<u64> = vec![10, 20, 20, 30];
         let read_lengths: Vec<u64> = vec![10, 20, 20, 30];
-        let keep_percent: f64 = 0.25;
+        let keep_percent: f64 = 0.1;
         let keep_bases: usize = 0;
         let indices = retain_indexed_quality_reads(read_qualities, read_lengths, keep_percent, keep_bases).unwrap();
 
@@ -609,7 +609,7 @@ mod tests {
         let indices = retain_indexed_quality_reads(read_qualities, read_lengths, keep_percent, keep_bases).unwrap();
 
         assert_eq!(indices.len(), 1);
-        assert_eq!(indices, vec![(1, 20)]);
+        assert_eq!(indices, vec![(3, 30), (2, 20)]);
     }
 
     #[test]
