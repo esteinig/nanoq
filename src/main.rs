4fd3e375170da06f51fdcbe38bb2892025d677cd
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-21 22:08:44 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -586,26 +586,6 @@ mod tests {
     // Needlecast based filters and stats
 
     #[test]
-    fn test_needlecast_stats_fq() {
-        let test_file = get_test_fq();
-        let (reads, base_pairs, read_lengths, read_qualities) = needlecast_stats(&test_file).unwrap();
-
-        assert_eq!(reads, 1);
-        assert_eq!(base_pairs, 12);
-        assert_eq!(read_lengths, vec![12]);
-    }
-
-    #[test]
-    fn test_needlecast_stats_fa() {
-        let test_file = get_test_fa();
-        let (reads, base_pairs, read_lengths, read_qualities) = needlecast_stats(&test_file).unwrap();
-        
-        assert_eq!(reads, 1);
-        assert_eq!(base_pairs, 12);
-        assert_eq!(read_lengths, vec![12]);
-    }
-
-    #[test]
     fn test_two_pass_filter_main_fq() {
         let test_file = get_test_fq();
         let completed = two_pass_filter(test_file, String::from("/dev/null"), 100.0, 0);
