8a4654ef7becfa3f9cf0a3746c7096d8183814e7
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-21 11:00:59 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -555,25 +555,25 @@ mod tests {
 
     fn get_test_fq() -> String {
         let root: String = get_root();
-        root.push_str(&String::from("test.fq"))
+        root.push_str(&String::from("test.fq"));
         return root;
     }
 
     fn get_test_fq_gz() -> String {
         let root: String = get_root();
-        root.push_str(&String::from("test.fq.gz"))
+        root.push_str(&String::from("test.fq.gz"));
         return root;
     }
 
     fn get_test_fa() -> String {
         let root: String = get_root();
-        root.push_str(&String::from("test.fa"))
+        root.push_str(&String::from("test.fa"));
         return root;
     }
 
     fn get_test_fa_gz() -> String {
         let root: String = get_root();
-        root.push_str(&String::from("test.fa.gz"))
+        root.push_str(&String::from("test.fa.gz"));
         return root;
     }
 
