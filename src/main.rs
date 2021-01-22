3a8980a520aa0e96e4d3ec3fdbeba1ad713d0dec
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 08:01:23 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -383,7 +383,7 @@ Median read quality: {:.1}
         eprintln!("Top ranking read qualities (read length)\n");
         for i in 0..top {
             let (read_index, qual) = indexed_qualities[i as usize];
-            eprintln!("{}. {:} bp ({:} bp)", i+1, qual, read_lengths[read_index]);
+            eprintln!("{}. Q{:} ({:} bp)", i+1, qual, read_lengths[read_index]);
         }
 
 
