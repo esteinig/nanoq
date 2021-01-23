2be72cffbaba99c6842b4c21eb3b6c79f01d72ba
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 16:39:09 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -437,7 +437,7 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
         let nreads = data[0].to_formatted_string(&Locale::en);
         let bp = data[1].to_formatted_string(&Locale::en); // Mbp
 
-        eprintln!(">{:<2}: {:<10} ({:}%) {:}", k, nreads, percent_reads, bp);
+        eprintln!(">{:<2}: {:} ({:}%) {:}", k, nreads, percent_reads, bp);
     }
     eprintln!("");
 
@@ -450,7 +450,7 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
         let bp =  data[1].to_formatted_string(&Locale::en);
 
 
-        eprintln!(">{:<8}: {:<10} ({:.4}%) {:}", k.to_formatted_string(&Locale::en), nreads, percent_reads, bp);
+        eprintln!(">{:<8}: {:} ({:.2}%) {:}", k.to_formatted_string(&Locale::en), nreads, percent_reads, bp);
     }
     eprintln!("");
 
