f4fb862200c254307d0d97168363885b116743e4
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 15:09:26 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use needletail::parser::Format;
 use needletail::parser::FastxReader;
 use std::collections::HashMap;
 use num_format::{Locale, ToFormattedString};
+use itertools::Itertools;
 
 fn command_line_interface<'a>() -> ArgMatches<'a> {
 
@@ -399,33 +400,31 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
     // Threshold summary prints with HashMaps
 
     let q_thresholds: Vec<f32> = vec![5.0, 7.0, 10.0, 15.0, 20.0];
-    let mut q_threshold_counts: HashMap<String, u64> = HashMap::new();
-    let mut q_threshold_lengths: HashMap<String, u64> = HashMap::new();
+    let mut q_threshold_data: HashMap<String, (u64, u64)> = HashMap::new();
 
     let l_thresholds: Vec<u64> = vec![200, 500, 1000, 5000, 10000, 50000, 100000, 1000000];
-    let mut l_threshold_counts: HashMap<String, u64> = HashMap::new();
-    let mut l_threshold_lengths: HashMap<String, u64> = HashMap::new();
+    let mut l_threshold_data: HashMap<String, (u64, u64)> = HashMap::new();
 
     for (l, q) in read_lengths.iter().zip(read_qualities.iter()){ // must be unsorted
         for tq in &q_thresholds {
             if q > &tq {
-                *q_threshold_counts.entry(tq.to_string()).or_insert(0) += 1;
-                *q_threshold_lengths.entry(tq.to_string()).or_insert(0) += l;
+                let (mut count, mut bp) = *q_threshold_counts.entry(tq.to_string()).or_insert((0, 0));
+                count += 1;
+                bp += l;
             }
         }
         for tl in &l_thresholds {
             if l > &tl {
-                *l_threshold_counts.entry(tl.to_string()).or_insert(0) += 1;
-                *l_threshold_lengths.entry(tl.to_string()).or_insert(0) += l;
+                let (mut count, mut bp) = *q_threshold_counts.entry(tq.to_string()).or_insert((0, 0));
+                count += 1;
+                bp += l;
             }
         }
     } 
 
     println!("{:?} {:?}", q_threshold_counts, l_threshold_counts);
 
-    for (i , (treshold, count)) in q_threshold_counts.enumerate(){
-        println!("{}", i);
-    }
+    for ()
 
 //     let length_str = eprintln!("
     
