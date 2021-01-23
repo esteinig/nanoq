dce8e9cd49a8db0b0df3d3d1b3beb0af8844722d
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 15:53:31 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -422,8 +422,8 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
         }
     } 
 
-    let mut qt_vec: Vec<(&String, &Vec<f32>)> = q_threshold_data.iter().collect();
-    let mut lt_vec: Vec<(&String, &Vec<u64>)> = l_threshold_data.iter().collect();
+    let mut qt_vec: Vec<(String, Vec<f32>)> = q_threshold_data.values().cloned().collect();
+    let mut lt_vec: Vec<(String, Vec<u64>)> = l_threshold_data.values().cloned().collect();
 
     qt_vec.sort_by(|a, b| (a.0.parse::<u64>().unwrap() ).cmp(&b.0.parse::<u64>().unwrap()));
     lt_vec.sort_by(|a, b| (a.0.parse::<u64>().unwrap() ).cmp(&b.0.parse::<u64>().unwrap()));
