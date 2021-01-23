0404970924b4f787f090afa8d7c112f96d7e9802
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 14:54:13 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -396,20 +396,40 @@ Median read quality:  {:.2}
 
 fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &u64) -> Result<(), Error> {
 
-    // Threshold summary prints
+    // Threshold summary prints with HashMaps
 
     let q_thresholds: Vec<f32> = vec![5.0, 7.0, 10.0, 15.0, 20.0];
     let mut q_threshold_counts: HashMap<String, u64> = HashMap::new();
-    for t in q_thresholds { q_threshold_counts.insert(t.to_string(), 0); };
+    let mut q_threshold_lengths: HashMap<String, u64> = HashMap::new();
+    for t in q_thresholds { 
+        q_threshold_counts.insert(t.to_string(), 0); 
+        q_threshold_lengths.insert(t.to_string(), 0); 
+    };
 
     let l_thresholds: Vec<u64> = vec![200, 500, 1000, 5000, 10000, 50000, 100000, 1000000];
-    let  mut l_threshold_bp: HashMap<String, u64> = HashMap::new();
-    for t in l_thresholds { l_threshold_bp.insert(t.to_string(), 0); };
+    let mut l_threshold_counts: HashMap<String, u64> = HashMap::new();
+    let mut l_threshold_lengths: HashMap<String, u64> = HashMap::new();
+    for t in l_thresholds { 
+        l_threshold_counts.insert(t.to_string(), 0);
+        l_threshold_lengths.insert(t.to_string(), 0);
+     };
 
-    for (l, q) in read_lengths.iter().zip(read_qualities.iter()){ // unsorted
-        println!("{} {}", l, q);
-
+    for (l, q) in read_lengths.iter().zip(read_qualities.iter()){ // must be unsorted
+        for t in q_thresholds {
+            if q > t {
+                q_threshold_counts[t] += 1;
+                q_threshold_lengths[t] += l;
+            }
+        }
+        for t in l_thresholds {
+            if l > t {
+                l_threshold_counts[t] += 1;
+                l_threshold_lengths[t] += l;
+            }
+        }
     } 
+
+    println!("{:?} {:?}", q_threshold_counts, l_threshold_counts);
 
 //     let length_str = eprintln!("
     
