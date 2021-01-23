603270ff1af5025e2044383db4a7bd15e10fadd7
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 16:56:34 +1000

    v0.2.0

--- paper/plot_benchmarks.py
+++ paper/plot_benchmarks.py
@@ -33,7 +33,7 @@ def plot_data(data: pandas.DataFrame) -> None:
         )
     )
 
-    data = data[data["ftype"] != "crab"]]  # exlude rust bio parser for now, slightly slower than needletail
+    data = data[data["ftype"] != "crab"]  # exlude bio parser for now, slightly slower than needletail
 
     filter_data = data[data['mode'] == 'filt']
     stats_data = data[data['mode'] == 'stat']
