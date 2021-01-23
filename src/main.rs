c91213b3dc5ec0cce018abd3d13c725ea7a5b435
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-01-23 15:43:10 +1000

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -422,8 +422,11 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
         }
     } 
 
+    q_threshold_data.sort_by(|a, b| (a.0 as f32).cmp(&b.0 as f32));
+    l_threshold_data.sort_by(|a, b| (a.0 as u64).cmp(&b.0 as u64));
+    
     eprintln!("Mean read quality thresholds\n");
-    for (t, data) in q_threshold_data.iter().sort_by(|a, b| (a.0 as f32).cmp(&b.0 as f32) ){
+    for (t, data) in q_threshold_data.iter() {
         let percent_reads: f64 = ((data[0] / reads) as f64)*100.0;
         let nreads = data[0].to_formatted_string(&Locale::en);
         let bp = data[1].to_formatted_string(&Locale::en);
@@ -433,7 +436,7 @@ fn print_thresholds(read_lengths: &Vec<u64>, read_qualities: &Vec<f32>, reads: &
     eprintln!("");
 
     eprintln!("Mean read length thresholds\n");
-    for (t, data) in l_threshold_data.iter().sort_by(|a, b| (a.0 as u64).cmp(&b.0 as u64) ) {
+    for (t, data) in l_threshold_data.iter() {
         let percent_reads: f64 = ((data[0] / reads) as f64)*100.0;
         let nreads = data[0].to_formatted_string(&Locale::en);
         let bp = data[1].to_formatted_string(&Locale::en);
